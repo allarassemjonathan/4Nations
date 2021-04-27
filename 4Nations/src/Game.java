cf1271e9fffe59d65c3b565abc44c89f5606b208
STEIGERNA20 <STEIGERNA20@5CG024DH4H.gcc.edu>
2021-04-27 14:16:24 -0400

    made a mistake

---
+++ 4Nations/src/Game.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+/**
+ * 
+ * @author ALLARASSEMJJ20
+ *
+ */
+public class Game {
+	
+	/**
+	 * Play the game test test test
+	 */
+	public void GameLoop() {
+		
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException {
+			GameManager gm = new GameManager();
+			gm.Manager();
+			
+	}
+
+}
